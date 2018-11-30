4d0eaf19aca89b2cc0512294db6a3733eedc8459
Ali Amin <ali.m.amin98@gmail.com>
2018-11-30 20:10:02 +0200

    Addd welcome message when launching the app

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,10 +1,15 @@
 #include "types.h"
 #include "minesweeper.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
 void playGame(void);
+void welcome(void);
 
 int main(void)
 {
+	welcome();
 	playGame();
 	return 0;
 }
@@ -13,4 +18,20 @@ void playGame(void)
 {
 	Minesweeper game = Minesweeper(4,4);
 	game.selectTile();
+}
+
+void welcome(void)
+{
+    std::string line;
+    std::ifstream inFile("welcome_art.txt");
+    if(!inFile.is_open())
+    {
+        std::cerr << "cant open file";
+        exit(0);
+    }
+    while(std::getline(inFile,line))
+    {
+        std::cout << line << std::endl;
+    }
+    inFile.close();
 }
