ec765d4b9db4eac790cc0da92906df60abe1a822
Ali Amin <ali.m.amin98@gmail.com>
2019-01-01 17:47:14 +0200

    Modularized files

---
+++ display_welcome.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+
+void welcome(void);
+
+void welcome(void)
+{
+    std::string line;
+    std::ifstream inFile("welcome_art.txt");
+    if(!inFile.is_open())
+    {
+        std::cerr << "cant open file";
+        exit(0);
+    }
+    while(std::getline(inFile,line))
+    {
+        std::cout << line << std::endl;
+    }
+    inFile.close();
+}
